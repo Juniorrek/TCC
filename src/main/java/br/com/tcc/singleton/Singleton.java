e36e838c4f7a528aec38874e6be554d84287dbba
brunogaleski <brunogaleskI@gmail.com>
2017-09-14 19:51:57 -0300

    local changes
    Signed-off-by: brunogaleski <brunogaleskI@gmail.com>

--- src/main/java/br/com/tcc/singleton/Singleton.java
+++ src/main/java/br/com/tcc/singleton/Singleton.java
@@ -1,26 +1,26 @@
 package br.com.tcc.singleton;
 
 public final class Singleton {
-    public static final String PDF_TO_TEXT_PATH = "C:/Program Files/xpdfbin-win-3.04/bin64/pdftotext.exe";
+    public static final String PDF_TO_TEXT_PATH = "C:/Program Files/pdftotext/bin64/pdftotext.exe";
     
     /***********\
     * SCRIPTS R *
     \***********/
-    public static final String EXTRACT_ABSTRACT = "C:/Users/David/Downloads/extractAbstract.R";
-    public static final String TIDYNATOR = "C:/Users/David/Downloads/tidynator.R";
-    public static final String FIND_TF_IDF = "C:/Users/David/Downloads/find_tf_idf.R";
-    public static final String FIND_OBJECTIVE = "C:/Users/David/Documents/Github/R-Scripts/findObjective.R";
+    public static final String EXTRACT_ABSTRACT = "C:/Users/bruno/Documents/R/Scripts/extractAbstract.R";
+    public static final String TIDYNATOR = "C:/Users/bruno/Documents/R/Scripts/tidynator.R";
+    public static final String FIND_TF_IDF = "C:/Users/bruno/Documents/R/Scripts/find_tf_idf.R";
+    public static final String FIND_OBJECTIVE = "C:/Users/bruno/Documents/R/Scripts/findObjective.R";
     
     /***********\
     *  UPLOADS  *
     \***********/
-    public static final String UPLOAD_DIR = "C:/Sistema_TCC/arquivos/artigos";
-    public static final String TEMP_DIR = "C:/Sistema_TCC/arquivos/temp";
+    public static final String UPLOAD_DIR = "C:/Users/bruno/Documents/up";
+    public static final String TEMP_DIR = "C:/Users/bruno/Documents/temp";
     
     /***********\
     *  DATABASE *
     \***********/
     public static final String DB_URL = "jdbc:mysql://localhost:3306/TCC?useSSL=false";
     public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "admin";
+    public static final String DB_PASSWORD = "";
 }
