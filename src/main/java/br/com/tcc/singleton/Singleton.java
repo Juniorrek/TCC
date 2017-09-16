3682f7387ba0939e487066433c0c815df1488b0e
JuniorRochaR <jr.1903@hotmail.com>
2017-09-16 15:28:40 -0300

    a

--- src/main/java/br/com/tcc/singleton/Singleton.java
+++ src/main/java/br/com/tcc/singleton/Singleton.java
@@ -1,28 +1,28 @@
 package br.com.tcc.singleton;
 
 public final class Singleton {
-    public static final String PDF_TO_TEXT_PATH = "C:/Program Files/xpdfbin-win-3.04/bin64/pdftotext.exe";
+    public static final String PDF_TO_TEXT_PATH = "C:/Users/Orestes/Desktop/TCC/pdftotext.exe";
     
     /***********\
     * SCRIPTS R *
     \***********/
-    public static final String EXTRACT_ABSTRACT = "C:/Users/David/Downloads/extractAbstract.R";
-    public static final String TIDYNATOR = "C:/Users/David/Downloads/tidynator.R";
-    public static final String FIND_TF_IDF = "C:/Users/David/Downloads/find_tf_idf.R";
-    public static final String FIND_OBJECTIVE = "C:/Users/David/Documents/Github/R-Scripts/findObjective.R";
-    public static final String FIND_SEGMENT = "C:/Users/David/Documents/Github/R-Scripts/findSegment.R";
-    public static final String ARTICLES_ANALYSIS = "C:/Users/David/Documents/Github/R-Scripts/articlesAnalysis.R";
+    public static final String EXTRACT_ABSTRACT = "C:/Users/Orestes/Desktop/TCC/R_files/extractAbstract.R";
+    public static final String TIDYNATOR = "C:/Users/Orestes/Desktop/TCC/R_files/tidynator.R";
+    public static final String FIND_TF_IDF = "C:/Users/Orestes/Desktop/TCC/R_files/find_tf_idf.R";
+    public static final String FIND_OBJECTIVE = "C:/Users/Orestes/Desktop/TCC/R_files/findObjective.R";
+    public static final String FIND_SEGMENT = "C:/Users/Orestes/Desktop/TCC/R_files/findSegment.R";
+    public static final String ARTICLES_ANALYSIS = "C:/Users/Orestes/Desktop/TCC/R_files/articlesAnalysis.R";
     
     /***********\
     *  UPLOADS  *
     \***********/
-    public static final String UPLOAD_DIR = "C:/Sistema_TCC/arquivos/artigos";
-    public static final String TEMP_DIR = "C:/Sistema_TCC/arquivos/temp";
+    public static final String UPLOAD_DIR = "C:/Users/Orestes/Desktop/TCC/SobAnalise";
+    public static final String TEMP_DIR = "C:/Users/Orestes/Desktop/TCC/SobAnalise";
     
     /***********\
     *  DATABASE *
     \***********/
     public static final String DB_URL = "jdbc:mysql://localhost:3306/TCC?useSSL=false";
     public static final String DB_USER = "root";
-    public static final String DB_PASSWORD = "admin";
+    public static final String DB_PASSWORD = "jrjr";
 }
